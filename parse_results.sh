63f5880ec28719a8b70fb01d7f3b7aa1da43512b
Anders Fugmann <anders@fugmann.net>
2024-02-28 22:00:33 +0100

    Update bench -> markdown script

--- parse_results.sh
+++ parse_results.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo "| name | protoc | plugin | ratio |"
-echo "|  --  |   --   |   --   |  --   |"
-grep 'ns/run' | while read _ name _ current _; do
+echo "| name | ocaml-protoc-plugin | ocaml-protoc | ratio |"
+echo "|  --  |          --         |       --     |  --   |"
+grep 'ns/run' | grep -v Varint | while read _ name _ current _; do
     if grep -q Plugin <(echo $name); then
         prev=$current
     else
         result=$(echo "scale=3; $prev / $current" | bc)
-        echo "| ${name%/*} | $current ns/run | $prev ns/run | $result |"
+        echo "| ${name%/*} | $prev ns/run | $current ns/run | $result |"
     fi
 done
